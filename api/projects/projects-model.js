fbc1133d15ebf3199787c34158ed7c6f22c588f7
Stephen Gary <sgary0@protonmail.com>
2020-09-11 11:59:58 -0400

    file structure set up

---
+++ api/projects/projects-model.js
@@ -0,0 +1,34 @@
+const db = require("../data/dbConfig.js");
+
+module.exports = {
+    addResource,
+    getResources,
+    addProject,
+    getProjects,
+    addTask,
+    getTask
+};
+
+function getResources() {
+    return 
+}
+
+function getProjects() {
+    return 
+}
+
+function getTask(project_id) {
+    return
+}       
+
+function addProject(data) {
+    return
+}
+
+function addResource(data) {
+    return
+}
+
+function addTask(data) {
+    return
+}
---
+++ api/projects/projects-router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const db = require('./projects-model.js');
+
+const router = express.Router();
+
+module.exports = router;
